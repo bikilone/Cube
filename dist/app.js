0e53e4ed433f9a72c4645a88d0d1e15c09e6003c
bikilone <milosbirovljevic@gmail.com>
2020-10-15 22:28:42 +0200

    Added opacity

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,14 @@ var H = document.getElementsByClassName('front')[0];
 var E = document.getElementsByClassName('bottom')[0];
 var A = document.getElementsByClassName('left')[0];
 var box = document.getElementsByClassName('box')[0];
+var hetzel = document.getElementsByClassName('hetzel')[0];
+var entertainment = document.getElementsByClassName('entertainment')[0];
+var architecture = document.getElementsByClassName('architecture')[0];
 
 H.addEventListener('mouseover', function() {
     box.classList.remove('rotate-e', 'rotate-a');
     box.classList.add('rotate-h');
+    hetzel.classList.add('show');
 });
 E.addEventListener('mouseover', function() {
     box.classList.remove('rotate-h', 'rotate-a');
