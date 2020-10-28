fc62e695054462e23a6c251976436657e6392fef
bikilone <milosbirovljevic@gmail.com>
2020-10-28 08:24:23 +0100

    Added animations

--- src/index.js
+++ src/index.js
@@ -7,16 +7,35 @@ var box = document.getElementsByClassName('box')[0];
 var hetzel = document.getElementsByClassName('hetzel')[0];
 var entertainment = document.getElementsByClassName('entertainment')[0];
 var architecture = document.getElementsByClassName('architecture')[0];
+var aImg = document.querySelector('.architecture img');
+var hImg = document.querySelector('.hetzel img');
+var eImg = document.querySelector('.entertainment img');
+var eventRunning = false;
+setTimeout(() => { 
+    architecture.style.width = aImg.offsetWidth + 'px';
+    hetzel.style.width = hImg.offsetWidth + 'px';
+    entertainment.style.width = eImg.offsetWidth + 'px';
+}, 0);
+// var perspective = document.getElementsByClassName('perspectives')[0];
+// var pA = document.getElementsByClassName('perspective-a')[0];
+// var pH = document.getElementsByClassName('perspective-h')[0];
+// var pE = document.getElementsByClassName('perspective-e')[0];
 
 H.addEventListener('mouseover', function() {
+    if(eventRunning) return;
+    eventRunning = true;
     addClassToBox('rotate-h');
     handleShowClass(hetzel);
 });
 E.addEventListener('mouseover', function() {
+    if(eventRunning) return;
+    eventRunning = true;
     addClassToBox('rotate-e');
     handleShowClass(entertainment);
 });
 A.addEventListener('mouseover', function() {
+    if(eventRunning) return;
+    eventRunning = true;
     addClassToBox('rotate-a');
     handleShowClass(architecture);
 });
@@ -26,7 +45,8 @@ function addClassToBox(classNotToRemove) {
     box.classList.add(classNotToRemove);
     setTimeout(() => {
         box.classList.remove(classNotToRemove);
-    }, 5500);
+        eventRunning = false;
+    }, 5000);
 }
 
 function handleShowClass(textToShow) {
@@ -34,6 +54,9 @@ function handleShowClass(textToShow) {
     texts.forEach(function(text) {
         if (text === textToShow) {
             text.classList.add('show');
+            setTimeout(() => {
+                text.classList.remove('show');
+            }, 4500);
         } else {
             text.classList.remove('show');
         }
