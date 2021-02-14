4279494ff0810e666a6910d2aba6b361ac33b3fd
bikilone <milosbirovljevic@gmail.com>
2021-02-14 13:57:28 +0100

    Edit webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,12 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "development",
   plugins: [
+    new StylelintPlugin({fix: true}),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
     })
